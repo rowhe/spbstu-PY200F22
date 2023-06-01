c2e35ce701aebee0d12f4b2f94f57eae784f44be
rowhe <dpopov.pro@yandex.ru>
2023-06-01 21:11:56 +0300

    linked list

--- Занятие 2. Атрибуты и методы/1. Практические задания/task1_system_user_attr/main.py
+++ Занятие 2. Атрибуты и методы/1. Практические задания/task1_system_user_attr/main.py
@@ -1,9 +1,12 @@
 class Glass:
     def __init__(self, capacity_volume: int, occupied_volume: int):
+        print(self.__dict__)
         self.capacity_volume = capacity_volume
         self.occupied_volume = occupied_volume
-
+        print(self.__dict__)
 
 if __name__ == "__main__":
     glass = Glass(200, 0)
     print(dir(glass))
+    print(glass.__class__)
+    print(glass.__dict__)
