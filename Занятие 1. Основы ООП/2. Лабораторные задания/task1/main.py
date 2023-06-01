246a084f170bdccca0921f33c5aa0ff9f94a1960
Denis Popov <dpopov@saber.games>
2023-06-01 17:09:18 +0300

    tasks 1.2.1 commit

--- Занятие 1. Основы ООП/2. Лабораторные задания/task1/main.py
+++ Занятие 1. Основы ООП/2. Лабораторные задания/task1/main.py
@@ -1,4 +1,7 @@
 # TODO Написать 3 класса с документацией и аннотацией типов
+class Table:
+    def __init__(self, color: str, weight):
+        color = color
 
 if __name__ == "__main__":
     # TODO работоспособность экземпляров класса проверить с помощью doctest
