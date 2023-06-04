3f8bb3baf8c9938506e630f1363c032b8f53e9e4
Denis <95048755+rowhe@users.noreply.github.com>
2023-06-04 22:16:06 +0300

    Lab 1 complited

--- Занятие 1. Основы ООП/2. Лабораторные задания/task1/main.py
+++ Занятие 1. Основы ООП/2. Лабораторные задания/task1/main.py
@@ -1,8 +1,46 @@
-# TODO Написать 3 класса с документацией и аннотацией типов
+# Написать 3 класса с документацией и аннотацией типов
+
 class Table:
-    def __init__(self, color: str, weight):
-        color = color
+    def __init__(self, color: str, weight: int):
+        """Данный класс описывает стол"""
+        self.color = color
+        self.weight = weight
+
+
+class Chair:
+    def __init__(self, body: str, height: int):
+        """Данный классс описывает стул"""
+        self.body = None
+        self.init_body(body)
+        self.height = None
+        self.init_height(height)
+
+    def init_body(self, body: str):
+        """Инициализируем кресло"""
+        ...
+    def init_height(self, height: int):
+        """Инициализируем высоту кресла"""
+        ...
+
+
+class Lamp:
+    def __init__(self, tint: str, direction: str):
+        """Данный класс описывает лампу"""
+        self.tint = None
+        self.init_tint(tint)
+        self.direction = None
+        self.init_direction(direction)
+
+    def init_tint(self, tint: str):
+        """Инициализируем оттенок света лампы"""
+        ...
+
+    def init_direction(self, direction: str):
+        """Инициализируем направление света лампы"""
+        ...
+
 
 if __name__ == "__main__":
-    # TODO работоспособность экземпляров класса проверить с помощью doctest
-    pass
+    import doctest
+    #  работоспособность экземпляров класса проверить с помощью doctest
+    doctest.testmod(Chair.init_body(1,2))
