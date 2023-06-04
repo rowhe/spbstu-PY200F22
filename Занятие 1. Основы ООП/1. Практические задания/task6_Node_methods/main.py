0618e8d8e99dca63584dc6cb2fe1b3764570165e
Denis <95048755+rowhe@users.noreply.github.com>
2023-06-04 21:49:22 +0300

    1.1.6 complited

--- Занятие 1. Основы ООП/1. Практические задания/task6_Node_methods/main.py
+++ Занятие 1. Основы ООП/1. Практические задания/task6_Node_methods/main.py
@@ -10,14 +10,14 @@ class Node:
         :param value: Любое значение, которое помещено в узел
         :param next_: следующий узел, если он есть
         """
-        self.value = value  # TODO добавить атрибуты
+        self.value = value  # добавить атрибуты
         self.next_ = next_
 
     def get_value(self) -> Any:
         """Метод, который возвращает значение атрибута value"""
-        return self.value # TODO вернуть значение узла
+        return self.value  # вернуть значение узла
 
-    # TODO добавить метод get_next
+    # добавить метод get_next
     def get_next(self):
         """Метод, которвый возвращает следующую Ноду"""
         return self.next_
@@ -27,5 +27,5 @@ if __name__ == '__main__':
     first_node = Node(1)  # первый узел
     second_node = Node(2)  # второй узел
 
-    # TODO с помощью метода распечатать значение первого узла
-    # TODO  с помощью метода распечатать следующий узел второго узла
+    print(first_node.get_value())# с помощью метода распечатать значение первого узла
+    print(second_node.get_next())# с помощью метода распечатать следующий узел второго узла
