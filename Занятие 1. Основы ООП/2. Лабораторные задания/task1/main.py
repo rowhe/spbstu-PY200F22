c16901d7aec7b72c8b6972cd1bfb1959929079eb
Denis Popov <dpopov@saber.games>
2023-06-05 21:33:42 +0300

    DZ 1

--- Занятие 1. Основы ООП/2. Лабораторные задания/task1/main.py
+++ Занятие 1. Основы ООП/2. Лабораторные задания/task1/main.py
@@ -1,13 +1,18 @@
 # Написать 3 класса с документацией и аннотацией типов
 
 class Table:
+    """class anotations"""
     def __init__(self, color: str, weight: int):
         """Данный класс описывает стол"""
         self.color = color
         self.weight = weight
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
+
 
 class Chair:
+    """Class describing Chair"""
     def __init__(self, body: str, height: int):
         """Данный классс описывает стул"""
         self.body = None
@@ -24,6 +29,7 @@ class Chair:
 
 
 class Lamp:
+    """Class describing Lamp"""
     def __init__(self, tint: str, direction: str):
         """Данный класс описывает лампу"""
         self.tint = None
@@ -44,3 +50,6 @@ if __name__ == "__main__":
     import doctest
     #  работоспособность экземпляров класса проверить с помощью doctest
     doctest.testmod(Chair.init_body(1,2))
+    print(Lamp.__doc__)
+    table = Table("red", 2)
+    print(table)
