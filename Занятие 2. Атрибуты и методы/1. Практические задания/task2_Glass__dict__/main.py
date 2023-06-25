14bff397e53add269f7742af66980d6b5bb92181
Denis <95048755+rowhe@users.noreply.github.com>
2023-06-25 13:48:12 +0300

    practice 2.1.2

--- Занятие 2. Атрибуты и методы/1. Практические задания/task2_Glass__dict__/main.py
+++ Занятие 2. Атрибуты и методы/1. Практические задания/task2_Glass__dict__/main.py
@@ -3,12 +3,11 @@ from typing import Union
 
 class Glass:
     def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
-        print(dict(self.__class__))
+        print(self.__dict__)
         self.capacity_volume = capacity_volume  # объем стакана
         self.occupied_volume = occupied_volume  # объем жидкости в стакане
-        print(dict(self.__class__))
+        print(self.__dict__)
 
 if __name__ == "__main__":
     glass = Glass(200, 100)
-    #print(type(glass) == glass.__init__)
-    glass
+    # print(type(glass) == glass.__init__)
