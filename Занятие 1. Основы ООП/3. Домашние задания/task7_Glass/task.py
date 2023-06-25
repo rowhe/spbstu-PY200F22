c525f2a857ab811096052b277ac5de87b9ba25d1
Denis <95048755+rowhe@users.noreply.github.com>
2023-06-25 12:11:04 +0300

    dz 1.3.7

--- Занятие 1. Основы ООП/3. Домашние задания/task7_Glass/task.py
+++ Занятие 1. Основы ООП/3. Домашние задания/task7_Glass/task.py
@@ -5,19 +5,28 @@ from typing import Union
 class Glass:
     def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
         self.capacity_volume = None
-        self.init_capacity_volume(capacity_volume)
+        self.init_capacity_volume(capacity_volume, occupied_volume)
         self.occupied_volume = None
-        self.init_occupied_volume(occupied_volume)
+        self.init_occupied_volume(capacity_volume, occupied_volume)
 
-    def init_capacity_volume(self, capacity_volume: Union[int, float]):
-        if not isinstance(capacity_volume, (int,float)):
+    def init_capacity_volume(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+        if not isinstance(capacity_volume, (int, float)):
             raise TypeError("wrong type")
         if capacity_volume < 0:
-            raise ValueError("wrong volume amount")
-        if capacity_volume
+            raise ValueError("capacity_volume cannot be less 0")
+        if capacity_volume < occupied_volume:
+            raise ValueError("occupied_cannot be greater than capacity_volume")
+
         self.capacity_volume = capacity_volume
 
-    def init_occupied_volume(self, occupied_volume: Union[int, float]):
+    def init_occupied_volume(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+        if not isinstance(occupied_volume, (int, float)):
+            raise TypeError("wrong type")
+        if occupied_volume < 0:
+            raise ValueError("occupied_volume cannot be less 0")
+        if capacity_volume < occupied_volume:
+            raise ValueError("occupied_volume cannot be greater than capacity_volume")
+
         self.occupied_volume = occupied_volume
 
 
